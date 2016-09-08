567101cf5f6f73ddfc9113bb71458576a8833501
Sergu <sergushov.1995@mail.ru>
2016-09-09 01:23:56 +0300

    day3 implimented

---
+++ Day3/StarWars/Controllers/StarwarsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StarWars.Controllers
+{
+    public class StarwarsController : Controller
+    {
+        // GET: Starwars
+        public ActionResult ChoseSide()
+        {
+            return View("Body");
+        }
+    }
+}
